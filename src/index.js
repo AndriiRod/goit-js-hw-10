b721d88249399b75138ce171906724882e4b9431
AndriiRod <rod.andriiw@gmail.com>
2023-04-24 16:34:27 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -42,35 +42,38 @@ function checkData(data) {
   }
 }
 function createCountryCardMarkup(data) {
+  refs.infoBox.classList.add('show-box');
   const { name, capital, population, flags, languages } = data[0];
 
-  const markUp = `<div class="info-wrap"><img src="${flags.svg}" alt="${
-    name.official
-  }"><h2>${name.official}</h2></div>
+  const markUp = `<div class="info-wrap"><img width="30" height="30" src="${
+    flags.svg
+  }" alt="${name.official}"><h2 class = "info-head" >${name.official}</h2></div>
   <ul class="info-list">
     <li class="info-item">
-    <p class="info-prop">Capital:</p>
-    <p class="info-text">${capital}</p>
+    <b>Capital:</b>
+    <p>${capital}</p>
   </li>
   <li class="info-item">
-    <p class="info-prop">Population:</p>
-    <p class="info-text">${population}</p>
+    <b>Population:</b>
+    <p>${population}</p>
   </li>
   <li class="info-item">
-    <p class="info-prop">Languages:</p>
-    <p class="info-text">${Object.values(languages).join(', ')}</p>
+    <b>Languages:</b>
+    <p>${Object.values(languages).join(', ')}</p>
   </li>
 </ul>`;
   refs.infoBox.insertAdjacentHTML('beforeend', markUp);
 }
 
 function createListMarkup(data) {
+  refs.countryList.classList.add('show-box');
+
   const markup = data.reduce(
     (acc, { flags, name }) =>
       (acc += `<li class="country-item"><img src=${flags.svg} alt="flag ${name.official}" width="25" height="25"> ${name.official}</li>`),
     ''
   );
-  refs.countryList.classList.add('show-country-list');
+
   refs.countryList.insertAdjacentHTML('beforeend', markup);
   setTimeout(() => {
     for (const li of refs.countryList.children) {
@@ -80,10 +83,11 @@ function createListMarkup(data) {
 }
 
 function resetListHtml() {
-  refs.countryList.classList.remove('show-country-list');
+  refs.countryList.classList.remove('show-box');
   refs.countryList.innerHTML = '';
 }
 
 function restInfoBox() {
+  refs.infoBox.classList.remove('show-box');
   refs.infoBox.innerHTML = '';
 }
